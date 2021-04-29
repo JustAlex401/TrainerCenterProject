8fb53bfbed53f4938ef75eafaf8157b53cdd5f16
JustAlex401 <plashko99@mail.ru>
2021-04-29 23:46:59 +0300

    Fixed icons for find calories

--- client/src/components/profile/userProfile/pagesForUser/listOfCalories/ListOfCalories.jsx
+++ client/src/components/profile/userProfile/pagesForUser/listOfCalories/ListOfCalories.jsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import './listOfCalories.css';
 import axios from 'axios';
 import CardForCalories from './cardForCalories/CardForCalories';
+import searchIcon from '../../../../../utils/images/search-in-mail.png';
+import notFoundIcon from '../../../../../utils/images/error.png';
 
 const ListOfCalories = () => {
 
   const [searchWord, setSearchWord] = useState();
   const [searchError, setSearchError] = useState();
   const [dishCalories, setDishCalories] = useState();
+  const [firstTime, setFirstTime] = useState(true);
 
   const changeHandler = event => {
     setSearchWord(event.target.value);
@@ -23,6 +26,7 @@ const ListOfCalories = () => {
       }
       setDishCalories(response.data.foods);
       console.log(response);
+      setFirstTime(false);
     } catch (err) {
       console.log(err);
       setSearchError(true);
@@ -37,6 +41,11 @@ const ListOfCalories = () => {
           <input placeholder="search" id="icon_prefix" type="text" class="validate" onChange={changeHandler}></input>
         </div>
       </div>
+      {firstTime &&
+          <div className='container center'>
+            <img src={searchIcon} style={{height: '50px'}}></img>
+            <p style={{color: 'white', fontSize: '20px'}}>You can find some dish ...</p>
+          </div>}
       <div className='col cards'>
         {!searchError ?
             dishCalories?.slice(0,10).map((item) =>{
@@ -47,8 +56,9 @@ const ListOfCalories = () => {
               }
             })
           :
-            <div className="row errorText">
-              <p style={{color: 'white', textAlign: 'center', fontSize: '20px', marginTop: '50px'}}>Unfortunately, we could not find this dish!..</p>
+            <div className="col container center errorText">
+              <img src={notFoundIcon} alt='000' style={{height: '70px'}}></img>
+              <p style={{color: 'white', textAlign: 'center', fontSize: '20px', marginTop: '20px'}}>Unfortunately, we could not find this dish!..</p>
             </div>
         }
       </div>
